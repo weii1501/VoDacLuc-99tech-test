8f044720aeec8fb60866ed8d15a417c9dc7c5710
weii1501 <vodacluc15@gmail.com>
2024-07-09 12:56:18 +0700

    first commit

---
+++ src/problem1/index.js
@@ -0,0 +1,71 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+var sum_to_n_a = function (n) {
+  let sum = 0;
+  for (var i = 1; i <= n; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+var sum_to_n_b = function (n) {
+  if (n === 1) {
+    return 1;
+  } else {
+    return n + sum_to_n_b(n - 1);
+  }
+};
+
+var sum_to_n_c = function (n) {
+  return Array.from({ length: n }, (_, i) => i + 1).reduce(
+    (acc, curr) => acc + curr,
+    0
+  );
+};
+
+function askQuestion(query) {
+  return new Promise((resolve) => {
+    rl.question(query, (answer) => {
+      resolve(parseInt(answer, 10));
+    });
+  });
+}
+
+async function main() {
+  const str = `Chọn hàm tính tổng:
+    1. sum_to_n_a
+    2. sum_to_n_b
+    3. sum_to_n_c
+Hàm bạn chọn: `;
+  let result;
+
+  let n = await askQuestion("Nhập một số nguyên: ");
+
+  let function_name = await askQuestion(str);
+
+  switch (function_name) {
+    case 1:
+      result = sum_to_n_a(n);
+      break;
+    case 2:
+      result = sum_to_n_b(n);
+      break;
+    case 3:
+      result = sum_to_n_c(n);
+      break;
+    default:
+      console.log("Hàm không tồn tại");
+      break;
+  }
+
+  console.log(`Kết quả: ${result}`);
+
+  process.exit(0);
+}
+
+main();
